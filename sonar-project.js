c30e9bf326ee360defbeebae4d550cc4cf8672ae
Puneetha T V <116545000+puneethatv@users.noreply.github.com>
2022-11-28 15:04:14 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: '172.31.35.254:9000/',
+  serverUrl: 'http://13.233.125.132:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
